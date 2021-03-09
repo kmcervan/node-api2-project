4e22295a3a8de8675ce4f9f57db5ccd223c09e78
karla-cervantes <cervantes.karla14@gmail.com>
2021-03-09 15:05:55 -0600

    first commit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+
+
+const express = require('express');
+
+// connecting routers will go here
+
+const server = express();
+server.use(express.json());
+
+// breakpoint routers will go here
+
+
+server.get('/', (req, res)=>{
+    res.send(`<h2>This API is ALIVEEEEE!`);
+});
+
+module.exports = server;
